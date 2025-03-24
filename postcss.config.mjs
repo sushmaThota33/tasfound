ed4834377c8011f7cbfc87807aa67e610c82f8d9
sushmaThota33 <thotasushma333@gmail.com>
2025-03-24 18:44:43 +0530

    Fix: CSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,8 @@
 const config = {
   plugins: ["@tailwindcss/postcss"], // Make sure it matches your project structure
   content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Include all files in the `app` directory
-    "./Components/**/*.{js,ts,jsx,tsx}", // Include components folder
+    "**/*.ts",
+    "**/*.tsx",
   ],
 };
 
