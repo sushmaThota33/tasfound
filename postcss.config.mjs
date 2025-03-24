71e1611f92d6d2e9c1853c2ce938f404fd650f8f
sushmaThota33 <thotasushma333@gmail.com>
2025-03-24 20:11:31 +0530

    Fix: Ensure Tailwind CSS is available during build

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,7 @@
 const config = {
-  plugins: ["@tailwindcss/postcss"], // Make sure it matches your project structure
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx,mdx}", // Include all files in the `app` directory
-    "./components/**/*.{js,ts,jsx,tsx}", // Include components folder
-  ],
+  plugins: {
+    '@tailwindcss/postcss': {},
+  },
 };
 
 export default config;
