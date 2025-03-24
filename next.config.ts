3b2164f8abcc263edcafba234cd91667882b02ce
sushmaThota33 <thotasushma333@gmail.com>
2025-03-24 17:55:59 +0530

    Fix Tailwind CSS issue

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,16 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  basePath: isProd ? "/tasfound" : "",
+  basePath: isProd ? '/tasfound' : '',
+  assetPrefix: isProd ? '/tasfound/' : '',
+  env: {
+    NEXT_PUBLIC_BASE_PATH: isProd ? "/tasfound" : ""
+  },
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+  output:'export',
+  distDir : "out",
   trailingSlash: true,
 };
 
